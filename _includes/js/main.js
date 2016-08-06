28b04371b21fd38974a4de78bd092600d5c1a1c6
Rob Ruana <rob@robruana.com>
2016-08-06 11:10:53 -0400

    Removes vestigial javascript

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -10,10 +10,8 @@ jQuery(document).ready(function($) {
 
     var $document = $(document),
         $window = $(window),
-        $body = $('body'),
         $navbar = $('#top-navbar'),
-        $navbarContent = $('#top-navbar-content'),
-        $slides = $('#slides');
+        $navbarContent = $('#top-navbar-content');
 
     $navbarContent.on('hide.bs.collapse', function () {
         $navbar.removeClass('expanded');
@@ -21,33 +19,6 @@ jQuery(document).ready(function($) {
 
     $navbarContent.on('show.bs.collapse', function () {
         $navbar.addClass('expanded');
-    });
-
-    $slides.on('movestart', function(e) {
-        // If the movestart is heading off in an upwards or downwards
-        // direction, prevent it so that the browser scrolls normally.
-        if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
-            e.preventDefault();
-        }
-    });
-
-    $slides.on('mousedown', function(e) {
-        e.preventDefault();
-        $body.addClass('grabbing');
-    });
-
-    $body.on('mouseup touchend', function() {
-        $body.removeClass('grabbing');
-    });
-
-    $slides.on('swiperight', function() {
-        $slides.carousel('prev');
-        $body.removeClass('grabbing');
-    });
-
-    $slides.on('swipeleft', function() {
-        $slides.carousel('next');
-        $body.removeClass('grabbing');
     });
 
     var updateNavbarTransparency = function() {
